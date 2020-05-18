8f9f17e47ad7e6f3f780430314eb327bc0025e79
arnurkuatov <arnurkuatov@gmail.com>
2020-05-18 17:06:31 +0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AuthModule} from './auth/auth.module';
+import {AuthHttpInterceptor} from './auth/auth-http-interceptor';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        AuthModule,
+        HttpClientModule
+    ],
+    providers: [
+        {provide: HTTP_INTERCEPTORS, useClass: AuthHttpInterceptor, multi: true}
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
